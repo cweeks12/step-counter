79687f39978f2549025f5f4d7af91ddab919ed15
Connor <connorweeks1@gmail.com>
2020-05-18 20:04:32 -0500

    Got it running

--- src/test/java/com/keepwalking/KeepWalkingPluginTest.java
+++ src/test/java/com/keepwalking/KeepWalkingPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.keepwalking;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class KeepWalkingPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(KeepWalkingPlugin.class);
 		RuneLite.main(args);
 	}
 }
